87c9bd2507f1401c0a68adf19472cb3d8f053f08
“LaBay” <labay@i.ua>
2016-05-23 01:23:08 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
-
-gulp.task('default', function() {
-  // place code for your default task here
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('.css//scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
 });
